f95acc3f587f28f4331563c20bca39e01604dffa
Nishant George Agrwal <nishant.george@hashedin.com>
2017-06-29 03:11:49 +0530

    updated npm entry point to bundle and changed file name

--- index.js
+++
@@ -1,4 +0,0 @@
-// start from entry point of typescript build output
-const {main} = require("./src/js/app.js")
-
-main()
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     __filename: false
   },
   entry: {
-      "renderer": './src/renderer/home.tsx',
+      "renderer": './src/renderer/index.tsx',
       "main": './src/electron-main/app.ts'
   },
   output: {
